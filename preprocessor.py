898f5299a48372196e29ad8a7e7f4aad6e44ab2f
Harikrishna Bhadiyadra <bhadiyadraharikrishna@gmail.com>
2024-05-15 12:05:40 +0530

    accepts all formats of data

--- preprocessor.py
+++ preprocessor.py
@@ -5,19 +5,28 @@ import re
 
 def extract_chat_data(chat_data):
     messages = []
-    pattern = r'^(\d{1,2}/\d{1,2}/\d{2,4}), (\d{1,2}:\d{2})(?: (?:am|pm)|\s-\s)(.+?): (.+)$'
+    patterns = [
+        r'^\[(\d{1,2}/\d{1,2}/\d{2,4}), (\d{1,2}:\d{2}:\d{2})\s?(?:AM|PM)?\] (.+?): (.+)$',
+        r'^(\d{1,2}/\d{1,2}/\d{2,4}), (\d{1,2}:\d{2})(?: (?:am|pm)|\s-\s)(.+?): (.+)$'
+    ]
     for line in chat_data.split('\n'):
-        try:
-            match = re.match(pattern, line)
-            if match:
-                date = match.group(1)
-                time = match.group(2)
-                sender = match.group(3)
-                message = match.group(4)
-                messages.append((date, time, sender, message))
-        except AttributeError:
-            # Handle the case when the pattern doesn't match
-            print(f"Warning: Pattern didn't match for line: {line}")
+        matched = False
+        for pattern in patterns:
+            try:
+                match = re.match(pattern, line)
+                if match:
+                    date = match.group(1)
+                    time = match.group(2)
+                    sender = match.group(3)
+                    message = match.group(4)
+                    messages.append((date, time, sender, message))
+                    matched = True
+                    break  # Break the loop if a match is found
+            except AttributeError:
+                # Handle the case when the pattern doesn't match
+                print(f"Warning: Pattern didn't match for line: {line}")
+        if not matched:
+            print(f"Warning: No pattern matched for line: {line}")
     return messages
 
 
